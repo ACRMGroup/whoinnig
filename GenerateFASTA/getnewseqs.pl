cd71644b42fbf472cc9e91a003cb43543a5d0fe6
Andrew Martin <Andrew C.R. Martin>
2016-11-25 17:09:07 +0000

    INN MedNet HTML parsing code now in a subroutine to rturn an array of hashes

--- GenerateFASTA/getnewseqs.pl
+++ GenerateFASTA/getnewseqs.pl
@@ -5,6 +5,17 @@ use strict;
 
 my @allData = ParseINNMedNet();
 
+PrintAllData(@allData);
+
+# ----------------------------------------------------------------------
+# Parses a INN MedNet .html results file to find URLs, IDs, Name,
+# Proposed list and Recommended list for each entry. Results are
+# returned as an array of hashes where the had indexes are 
+# - url
+# - id
+# - name
+# - proposed
+# - recommended
 sub ParseINNMedNet
 {
     my $inRow = 0;
@@ -17,49 +28,52 @@ sub ParseINNMedNet
     {
         chomp;
 
-        if(/\<tr.*\>/)
+        if(/\<tr.*\>/)          # Start of a table row
         {
             $inRow  =  1;
             $column = -1;
             @data   = ();
         }
-        elsif(/\<\/tr\>/)
+        elsif(/\<\/tr\>/)       # End of a table row
         {
             $inRow = 0;
             CleanData(\@data);
             StoreData(\@allData, \@data);
         }
-        elsif($inRow)
+        elsif($inRow)           # In a row
         {
-            if(/\<td\>/)
+            if(/\<td\>/)        # In a <td>
             {
                 $column++;
-                if(/\<td\>(.*)\<\/td\>/)
+                if(/\<td\>(.*)\<\/td\>/) # <td>.....</td>
                 {
                     $data[$column] = $1;
                 }
-                else
+                else                     # <td> (data on following lines)
                 {
                     $inData = 1;
                     $data[$column] = '';
                 }
             }
-            elsif(/\<\/td\>/)
+            elsif(/\<\/td\>/)   # </td>
             {
                 $inData = 0;
             }
-            elsif($inData)
+            elsif($inData)      # Between <td> and </td>
             {
                 s/^\s+//;
                 s/\s+$//;
-                $data[$column] .= $_;
+                $data[$column] .= " $_";
             }
         }
     }
     return(@allData); 
 }
 
-
+# ----------------------------------------------------------------------
+# Cleans up the data array retrieved from an entry.  Slot[0] contains
+# the URL so we extract that - NULL if missing. Other slots contain
+# markup which we wish to remove as well as extraneous whitespace.
 sub CleanData
 {
     my($aData) = @_;
@@ -90,14 +104,10 @@ sub CleanData
 }
 
 
-sub PrintData
-{
-    my($aData) = @_;
-
-    printf("%-20s %5d %-35s %4d %4d\n",
-           $$aData[0], $$aData[2], $$aData[3], $$aData[4], $$aData[5]);
-}
-
+# ----------------------------------------------------------------------
+# Takes a reference to the array of hashes and a reference to the data
+# array for a given line of data. Constructs a hash out of the latter
+# and pushes it onto the data array
 sub StoreData
 {
     my($aAllData, $aData) = @_;
@@ -105,3 +115,19 @@ sub StoreData
     push @$aAllData, {url=>$$aData[0], id=>$$aData[2], name=>$$aData[3],
                       proposed=>$$aData[4], recommended=>$$aData[5]};
 }
+
+
+# ----------------------------------------------------------------------
+# Prints all items in the data array
+sub PrintAllData
+{
+    my(@allData) = @_;
+
+    foreach my $data (@allData)
+    {
+        printf("%-20s %5d %-35s %4d %4d\n",
+               $$data{'url'}, $$data{'id'}, $$data{'name'}, 
+               $$data{'proposed'}, $$data{'recommended'});
+    }
+}
+
